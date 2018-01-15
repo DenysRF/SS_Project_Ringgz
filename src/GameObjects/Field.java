e7067055dd196fbf6da0c2ae974f01470744330e
Thomas Stouten <t.stouten@student.utwente.nl>
2018-01-15 16:24:11 +0100

    field is zo goed als af, alleen de check wie de "eigenaar" is van een field moet nog geimplementeerd worden.

--- src/GameObjects/Field.java
+++ src/GameObjects/Field.java
@@ -3,12 +3,15 @@ package GameObjects;
 import Players.Player;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Field {
 
     public static final int MAX_SPACE = 5;
 
     private Piece[] fieldContent;
+    private int place;
 
     // Constructor initializes fieldContent at size MAX_SPACE with values null
     public Field() {
@@ -23,22 +26,32 @@ public class Field {
 
     // Set the content of a field
     public void setFieldContent(Piece piece) {
-        // TODO
+        place = piece.getSize();
+        if (fieldContent[place].equals(null)) {
+            fieldContent[place] = piece;
+        }
     }
+
 
     // Checks whether the piece fits in the field
     public boolean isValidMove(Piece piece) {
-        // TODO
-        return false;
+        place = piece.getSize();
+        if (isEmpty() || fieldContent[place].equals(null) && fieldContent[0].equals(null)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Checks if anything fits in the field at all
     public boolean isFull() {
         int count = 0;
         for (Piece p : fieldContent) {
-            count++;
             if (p instanceof Base) {
                 return true;
+            }
+            if (p instanceof Piece) {
+                count++;
             }
         }
         if (count == MAX_SPACE) {
@@ -47,9 +60,39 @@ public class Field {
         return false;
     }
 
+    public boolean isEmpty() {
+        int count = 0;
+        for (Piece p : fieldContent) {
+            if (!p.equals(null)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     // Returns the player with the majority of the field or null
     public Player majority() {
-        // TODO
+        Player owner;
+        int colour;
+        String player;
+        int[] amount = new int[2];
+        Map<Player, int[]> score= new HashMap<Player, int[]>();
+
+        int count = 1;
+        while (count < MAX_SPACE) {
+           owner = fieldContent[count].getOwner();
+           colour = fieldContent[count].getColour();
+           if(!score.get(owner).equals(null)){
+               int[] tempscore = score.get(owner);
+               tempscore[colour] = tempscore[colour]+1;
+               score.put(owner, tempscore);
+           }else{
+               int[] tempscore = amount;
+               tempscore[colour] = 1;
+               score.put(owner, tempscore);
+           }
+        }
+        /// TODO: 15-Jan-18 itterate over hashmap to define winner 
         return null;
     }
 
--- src/GameObjects/Piece.java
+++ src/GameObjects/Piece.java
@@ -5,12 +5,25 @@ import Players.Player;
 public abstract class Piece {
 
     private Player owner;
+    private int size;
+    private int colour;
 
-    public Piece(Player owner) {
+    public Piece(Player owner, int Size, int colour) {
         this.owner = owner;
+        this.size = size;
+        this.colour = colour;
     }
 
     public Player getOwner() {
         return owner;
     }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getColour() {
+        return colour;
+    }
+
 }
